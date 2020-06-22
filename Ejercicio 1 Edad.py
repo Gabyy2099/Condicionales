7b609cd3b0715df2da59160e9fac090fdbd6cbdc
Gabriel Avila <pablogabriel2099@gmail.com>
2020-06-22 15:17:06 -0300

    Delete Ejercicio 1 Edad.py

--- Ejercicio 1 Edad.py
+++
@@ -1,8 +0,0 @@
-# Escribir un programa que solicite la edad al usuario y mostrar si es mayor de edad o no
-
-# Codigo:
-edad = int(input('Ingrese su edad:'))
-if edad >= 18:
-    print('Usted es mayor de edad')
-else:
-    print('Usted es menor de edad')
